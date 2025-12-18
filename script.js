370bac524b4698a4efde84a6540910afbfadf8c7
OmkarKovvali <okovvali5@gmail.com>
2025-12-18 15:56:08 -0500

    fixed code look and images formatting

--- script.js
+++ script.js
@@ -31,3 +31,4 @@ document.addEventListener("DOMContentLoaded", function() {
     loadComponent("#header-container", "components/header.html");
     loadComponent("#footer-container", "components/footer.html");
 });
+
